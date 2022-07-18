4eb16acda7ea7c865c8778651a47c678bee5ccd5
akhilsuryam <99366510+akhilsuryam@users.noreply.github.com>
2022-07-18 07:33:07 +0530

    Update customer.controller.js

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -1,67 +1,36 @@
-var customers = {
-				customer1: {
-					firstname: "Jack",
-					lastname: "Davis",
-					age: 25,
-					id: 1
-				},
-				customer2: {
-					firstname: "Mary",
-					lastname: "Taylor",
-					age: 37,
-					id: 2
-				},
-				customer3: {
-					firstname: "Peter",
-					lastname: "Thomas",
-					age: 17,
-					id: 3
-				},
-				customer4: {
-					firstname: "Peter",
-					lastname: "Thomas",
-					age: 17,
-					id: 4
-				}
-			}
-
-exports.create = function(req, res) {
-	var newCustomer = req.body;
-    customers["customer" + newCustomer.id] = newCustomer;
-	console.log("--->After Post, customers:\n" + JSON.stringify(customers, null, 4));
-    res.end("Post Successfully: \n" + JSON.stringify(newCustomer, null, 4));
-};
-
-exports.findAll = function(req, res) {
-    console.log("--->Find All: \n" + JSON.stringify(customers, null, 4));
-    res.end("All Customers: \n" + JSON.stringify(customers, null, 4));  
-};
-
-exports.findOne = function(req, res) {
-    var customer = customers["customer" + req.params.id];
-    console.log("--->Find customer: \n" + JSON.stringify(customer, null, 4));
-    res.end( "Find a Customer:\n" + JSON.stringify(customer, null, 4));
-};
-
-exports.update = function(req, res) {
-	var id = parseInt(req.params.id);
-	var updatedCustomer = req.body; 
-	if(customers["customer" + id] != null){
-		// update data
-		customers["customer" + id] = updatedCustomer;
-
-		console.log("--->Update Successfully, customers: \n" + JSON.stringify(customers, null, 4))
-		
-		// return
-		res.end("Update Successfully! \n" + JSON.stringify(updatedCustomer, null, 4));
-	}else{
-		res.end("Don't Exist Customer:\n:" + JSON.stringify(updatedCustomer, null, 4));
-	}
-};
-
-exports.delete = function(req, res) {
-	var deleteCustomer = customers["customer" + req.params.id];
-    delete customers["customer" + req.params.id];
-    console.log("--->After deletion, customer list:\n" + JSON.stringify(customers, null, 4) );
-    res.end( "Deleted customer: \n" + JSON.stringify(deleteCustomer, null, 4));
-};
+accountRoutes.post('/account/addaccount', (req, res) => {
+ 
+    var existAccounts = getAccountData()
+    const newAccountId = Math.floor(100000 + Math.random() * 900000)
+ 
+    existAccounts[newAccountId] = req.body
+   
+    console.log(existAccounts);
+    saveAccountData(existAccounts);
+    res.send({success: true, msg: 'account added successfully'})
+})
+// Update - using Put method
+accountRoutes.put('/account/:id', (req, res) => {
+  var existAccounts = getAccountData()
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    const accountId = req.params['id'];
+    existAccounts[accountId] = req.body;
+    saveAccountData(existAccounts);
+    res.send(`accounts with id ${accountId} has been updated`)
+  }, true);
+});
+// delete - using delete method
+accountRoutes.delete('/account/delete/:id', (req, res) => {
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    var existAccounts = getAccountData()
+    const userId = req.params['id'];
+    delete existAccounts[userId]; 
+    saveAccountData(existAccounts);
+    res.send(`accounts with id ${userId} has been deleted`)
+  }, true);
+})
+// Read - get all accounts from the json file
+accountRoutes.get('/account/list', (req, res) => {
+  const accounts = getAccountData()
+  res.send(accounts)
+})
